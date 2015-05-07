07f231e88865275fed58029f0338a1a303c65819
Frederik Boi <boifrederik@telenet.be>
2015-05-07 09:37:13 +0200

    Eerste versie javanca

---
+++ public/theme/javanca/config.rb
@@ -0,0 +1,10 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'scss'
+images_dir = 'images'
+javascripts_dir = 'js'
+relative_assets = true
+line_comments = true
+output_style = :expanded
+# output_style = :compressed
